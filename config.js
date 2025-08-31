b75be63d2d6f425c46fd0b9961854a85eeb3c0b1
The Dentist <ilCapo01@example>
2025-08-31 19:37:37 +0300

    config+dynamic

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
+import { * } from 'https://code.jquery.com/jquery-3.7.1.slim.min.js';
 
 const config = {
 	symbol: "/symbol.jpg",
 	symbolAlt: "Gnosis via Trivium et Quadrivium",
 	quote: "Quote",
-	title: "TITLE ?"
+	title: "TITLE ?",
+	authorName: "SOMEONE"
 };
+
+
+const essays = function(id) {
+	
+
+	// Load the essays.json
+	// Pass the ID of the essay you want to get as a parameter
+	//Return an Array with all the essay's data, including the essay itself
+	//
+}
 
 
 
