9e408b909482924c5aa2734c080537faf3172b2b
The Dentist <ilCapo01@example>
2025-09-01 14:30:45 +0300

    clean url

--- config.js
+++ config.js
@@ -13,14 +13,14 @@ const getEssay = function(id) {
 	// Load/read the essays.json
 	// Pass the ID of the essay you want to get as a parameter
 	//Return an Array with all the essay's data, including the essay itself
-	return id
+	return id;
 }
 
 // window.location.href is passed as parameter
 const getPage = function(loc) {
 	const url=loc.href;
 	const cleanURL = location.href.replace('https://', '');
-	return cleanURL.split('/')[1].split('?')[1].split('=');
+	return cleanURL.split('/')[1].split('?')[1].split('=')[1];
 }
 
 
